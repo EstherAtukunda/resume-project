d4969fadb65273bacac09e6b3421b2847059003b
EstherAtukunda <atukundaesther04@gmail.com>
2025-09-17 09:18:30 -0500

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,14 @@
+// Define variables
+
+let myName = "Esther Atukunda";
+let title = "Student";
+let education = "Oral Roberts University";
+let experience = "Tutor, Helpdesk, and programming";
+let skills = ["Skating", "Programming", "Teaching"];
+
+// Connect Variables to the HTML (ligaments)
+document.getElementById("my-name").textContent = myName;
+document.getElementById("title").textContent = title;
+document.getElementById("education").textContent = education;
+document.getElementById("experience").textContent = experience;
+document.getElementById("skills").textContent = skills;
